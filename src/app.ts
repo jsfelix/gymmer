a7601e2e285614a0c3c03fb43fc9101835624478
Jefferson Felix <jefferson@felixtech.dev>
2023-07-25 23:40:05 -0300

    feat: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify({ logger: true })
+
+const prisma = new PrismaClient()
+
+prisma.user.findMany().then((user) => console.log(user))
